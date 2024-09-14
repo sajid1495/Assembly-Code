a0c230e70ae9526a62a3422f38e3c6a0bef20776
MD.SAJEDUL ISLAM <m.sajid1495@gmail.com>
2024-09-14 14:05:40 +0600

    adding newline feature

--- Addition of Two Numbers/add.asm
+++ Addition of Two Numbers/add.asm
@@ -8,22 +8,59 @@ main proc
     MOV AX, @data
     MOV DS, AX
     
+    ;taking input of 1st number
     MOV AH, 1
     INT 21H 
     MOV A, AL 
-    SUB A, 30h 
+    SUB A, 30h
     
+    ;printing space
+    MOV DL, 20H
+    MOV AH, 2
+    INT 21H  
+     
+    
+    ;taking input of 2nd number
     MOV AH, 1
     INT 21H  
     MOV B, AL
     SUB B, 30h 
     
+    ;adding numbers
     MOV AL, A
-    ADD AL, B 
+    ADD AL, B
+    MOV BL, AL 
     
-    MOV DL, AL
+    ;printing newline
+    MOV DL, 10
+    MOV AH, 2
+    INT 21H
+    MOV DL, 08
+    MOV AH, 2
+    INT 21H
+    
+    ;printing result
+    MOV DL, BL
     ADD DL, 30H
+    MOV AH, 2
+    INT 21H  
     
+    ;substracting numbers
+    MOV AL, A
+    SUB AL, B 
+    MOV BL, AL
+    
+    ;printing newline
+    MOV DL, 10
+    MOV AH, 2
+    INT 21H
+    MOV DL, 08
+    MOV AH, 2
+    INT 21H
+    
+    ;printing result
+    MOV DL, BL
+    ADD DL, 30H
     MOV AH, 2
     INT 21H
     
